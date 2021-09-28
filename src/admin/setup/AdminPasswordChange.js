e4308613c614c43b3d14e0dac3e15191ef87517d
Sidath Samarasinghe <sidaath97@gmail.com>
2021-09-28 20:01:48 +0530

    cleaned up admin initial login

--- src/admin/setup/AdminPasswordChange.js
+++ src/admin/setup/AdminPasswordChange.js
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect,useState } from 'react'
+import React, { useContext,useState } from 'react'
 import axios from 'axios'
 import { AppData } from '../../home/Home'
 import { useHistory } from 'react-router'
@@ -18,19 +18,8 @@ export default function AdminPasswordChange({logo, orgDeet}) {
     //use passwordMatch to display an error? otherwise can remove this.
     //const [passwordMatch, setMatch] = useState(undefined)
 
-    //set to true when server has responded with successfull update.
-    const [detailsUpdated, setDetailsUpdated] = useState(false)
-    const [logoUpdated, setLogoUpdated] = useState(false)
 
-    //render loading screen if this is true, while waiting for server response.
     const [loading, setLoading] = useState(false)
-
-    useEffect(()=>{
-        if(detailsUpdated && logoUpdated){
-            updatePassword(logoUpdated, detailsUpdated)
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[detailsUpdated, logoUpdated])
 
     function submit(){
         if(password !== password2){
@@ -39,51 +28,27 @@ export default function AdminPasswordChange({logo, orgDeet}) {
             return
         }
 
+        setLoading(true)
+
         //post to server
-        //first update details, then update password.
+        //first update details, then logo, then update password.
         const config = { headers: { Authorization: `Bearer ${token}` } }
+        const data = {adminEmail:email, adminPassword:password}
 
         //update org details
         axios.put(`${BACKEND_URL}/admin/organization-info`, orgDeet, config)
         .then((response)=>{
             console.log("details updates in server -> ", response)
             if(response.status === 200){
-                setDetailsUpdated(true)
-                updatePassword(logoUpdated, detailsUpdated)
             }
+            return axios.put(`${BACKEND_URL}/admin/organization-logo`, logo, config)
         })
-        .catch((er)=>{
-            console.log('error updating details. ',er)
-        })
-
-        
-        //update logo
-        axios.put(`${BACKEND_URL}/admin/organization-logo`, logo, config)
         .then((response)=>{
             console.log("success updating logo. ", response)
             if(response.status===200){
-                setLogoUpdated(true)
-                updatePassword(logoUpdated, detailsUpdated)
             }
+            return axios.put(`${BACKEND_URL}/admin/credentials`,data, config)
         })
-        .catch((er)=>{
-            console.log("error updating logo ",er)
-        })
-
-    }
-
-    //update password if both org deets and logo are saved.
-    //cos updating password sets initialized to true.
-    function updatePassword(serverGotLogo, serverGotDetails){
-        if(!serverGotLogo || !serverGotDetails){
-            return
-        }
-
-        setLoading(true)
-        const data = {adminEmail:email, adminPassword:password}
-        const config = { headers: { Authorization: `Bearer ${token}` } }
-
-        axios.put(`${BACKEND_URL}/admin/credentials`,data, config)
         .then((response)=>{
             console.log("updated password. ",response)
             if(response.status===200){
@@ -93,9 +58,7 @@ export default function AdminPasswordChange({logo, orgDeet}) {
             }
         })
         .catch((er)=>{
-            console.log('couldnt update password', er)
-            alert('password update failed. woops.')
-            return
+            console.log('error updating details. ',er)
         })
     }
 

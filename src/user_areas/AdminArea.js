f0227e8a2c09137b5f0694ae2cca9902b9c7b05f
Sidath Samarasinghe <sidaath97@gmail.com>
2021-09-29 19:42:52 +0530

    admin area logout

--- src/user_areas/AdminArea.js
+++ src/user_areas/AdminArea.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router'
+import { USER_STORAGE_KEY } from '../app_data/constants'
+import { removeUserFromStorage } from '../utility/functions'
 
 export default function AdminArea() {
+    const history = useHistory()
+
+    function handleLogOut(){
+        removeUserFromStorage(USER_STORAGE_KEY)
+        history.push('/')
+    }
+
 
     return (
-       <h1>Admin Area</h1>
+       <div>
+           <h1>Admin Area</h1>
+           <button onClick={handleLogOut}>logout</button>
+       </div>
+
     )
 }

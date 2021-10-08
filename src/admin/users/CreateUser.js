c32d8ba098c762c7e329bf5bd06a7eaf6f3ac4b2
Sidath Samarasinghe <sidaath97@gmail.com>
2021-10-09 00:01:03 +0530

    fetch division and role options for new user

--- src/admin/users/CreateUser.js
+++ src/admin/users/CreateUser.js
@@ -1,13 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import LoadingScreen from '../../utility/LoadingScreen'
+import { getDivisions } from '../divisions/functions'
 import InputField from '../other/InputField'
+import { getRoles } from '../roles_and_permissions/functions'
 
 export default function CreateUser() {
-
     const [name, setName] = useState('')
+    const [divisionOptions, setDivisionOptions] = useState([])
+    const [roleOptions, setRoleOptions] = useState([])
+    
+    //loading screen
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        fetchOptions()
+        async function fetchOptions(){
+            const divisions = await getDivisions()
+            const roles = await getRoles()
+            let error = 'Failed to get '
+            !divisions ? error +='divisions ': error+=''
+            !roles ? error +='and roles' : error+=''
+            if(!divisions || !roles){
+                alert(`${error}`)
+                setLoading(false)
+                return
+            }
+            setDivisionOptions(divisions)
+            setRoleOptions(roles)
+            setLoading(false)
+        }
+        return(()=>{
+            setRoleOptions([])
+            setDivisionOptions([])
+        })
+    },[])
+
+    function test(){
+        console.log("divisions options ",divisionOptions)
+        console.log('role options ', roleOptions)
+    }
+
+    if(loading){
+        return <LoadingScreen message='getting organization details...'/>
+    }
+
     return (
         <div>
             <h1>Create User</h1>
             <InputField type='text' input={name} setInput={setName} placeholder='Name' />
+            <button onClick={test}>Click</button>
         </div>
     )
 }
